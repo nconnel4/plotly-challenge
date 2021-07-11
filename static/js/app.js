7439f7e040344360201063de357ae305ce7f8b3b
nconnel4 <nconnel4@hotmail.com>
2021-07-11 18:15:47 -0400

    Add dropdown populate and change

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,32 @@
-d3.json("samples.json").then(function(data) {
-    console.log(data);
-})
+function init() {
+    
+    //load data from samples.json
+    d3.json("samples.json").then(function(data) {
+        var names = data.names;
+        console.log(names);
+
+        var dropDown = d3.select("#selDataset");
+
+        dropDown
+            .selectAll("options")
+            .data(names)
+            .enter()
+            .append("option")
+            .text(function (d) { return d; })
+            .attr("value", function (d) { return d; });
+
+        optionChanged();
+    })
+
+}
+
+
+function optionChanged() {
+
+    var dropDown = d3.select("#selDataset");
+    var subjectId = dropDown.property("value");
+
+    console.log(subjectId)
+}
+
+init();
