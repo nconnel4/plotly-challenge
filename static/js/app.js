3c33d6f4be21456b3e65b07f841735404318f640
nconnel4 <nconnel4@hotmail.com>
2021-07-11 18:56:35 -0400

    Add demographics info for subject

--- static/js/app.js
+++ static/js/app.js
@@ -20,13 +20,47 @@ function init() {
 
 }
 
+function getDemographics(id) {
+    d3.json("samples.json").then(data => {
+        var metaData = data.metadata;
+
+        // filter demographic data for subject id selected
+        var testSubject = metaData.filter(subject => {
+            return subject.id == id;
+        });
+
+        // assign variable for demographic info box
+        var demographicBox = d3.select("#sample-metadata");
+
+        // clear demographic info box
+        demographicBox.selectAll("*").remove();
+
+        for(var [key, value] of Object.entries(testSubject[0])) {
+            demographicBox
+                .insert("h5")
+                .text(`${key.toUpperCase()}: ${value}`);
+        };
+
+    });
+}
+
+function getSamples(id) {
+    d3.json("samples.json").then(function(data) {
+        console.log(data)
+    })
+}
+
 
 function optionChanged() {
 
     var dropDown = d3.select("#selDataset");
     var subjectId = dropDown.property("value");
 
-    console.log(subjectId)
+    console.log(subjectId);
+    getSamples(subjectId);
+    getDemographics(subjectId);
 }
+
+
 
 init();
